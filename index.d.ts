7a742dbae722d7a2ef56f9ddce7eb050d35fc3bb
Ilya Ageev <maniak89s@gmail.com>
2018-09-25 18:04:29 +0300

    Add support recoverPersonalSignature

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,9 @@ declare module "eth-sig-util" {
   import { Buffer } from "safe-buffer"
 
   export function concatSig(v: number, r: Buffer, s: Buffer): Buffer
+  export interface MsgParams {
+    data: any;
+    sig: any;
+  }
+  export function recoverPersonalSignature(msgParams: MsgParams): string
 }
