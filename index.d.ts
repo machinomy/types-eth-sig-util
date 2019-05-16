719eee09b94c5e423f2c032f15b9bb9f40bfdf94
Sergey Ukustov <sergey.ukustov@machinomy.com>
2019-05-16 13:07:08 +0800

    Cover personalSign and recoverPersonalSignature

--- index.d.ts
+++ index.d.ts
@@ -2,4 +2,6 @@ declare module "eth-sig-util" {
   import { Buffer } from "safe-buffer"
 
   export function concatSig(v: number, r: Buffer, s: Buffer): Buffer
+  export function personalSign (privateKeyBuffer: Buffer, msgParams: { data: string }): string
+  export function recoverPersonalSignature(msgParams: { data: string, sig: string }): string
 }
